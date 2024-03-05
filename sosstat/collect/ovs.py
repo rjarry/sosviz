rjarry/sosviz

commit 905647dda10e3c95d402fb49688bee943d872294

ovs: assume default interface type is tap
Signed-off-by: Robin Jarry <rjarry@redhat.com>

sosstat/collect/ovs.py
@@ -22,7 +22,7 @@ PORT_RE = re.compile(
 IFACE_RE = re.compile(
     r"""
     ^\s+Interface\s+(?P<name>.+)\n
-        ^\s+type:\s+(?P<type>.+)\n
+        (?:^\s+type:\s+(?P<type>.+)\n)?
         (?:^\s+options:\s+(?P<options>.+)\n)?
     """,
     re.VERBOSE | re.MULTILINE,
@@ -69,7 +69,7 @@ def ovs_ports(ovs, path):
                 ifaces = D()
                 for m in IFACE_RE.finditer(match.group("ifaces")):
                     name = strip_quotes(m.group("name"))
-                    ifaces[name] = D(name=name, type=m.group("type"))
+                    ifaces[name] = D(name=name, type=m.group("type", "tap"))
                     if m.group("options"):
                         ifaces[name].options = cast_value(m.group("options"))
                 if not ifaces:

commit 4b338d9280eaed52575955e1c88654e24b26de93

ovs: assume default interface type is tap (fix)
Regular expression match.group() does not take a default value argument.

Fixes: 905647dda10e ("ovs: assume default interface type is tap")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

sosstat/collect/ovs.py
@@ -69,7 +69,7 @@ def ovs_ports(ovs, path):
                 ifaces = D()
                 for m in IFACE_RE.finditer(match.group("ifaces")):
                     name = strip_quotes(m.group("name"))
-                    ifaces[name] = D(name=name, type=m.group("type", "tap"))
+                    ifaces[name] = D(name=name, type=m.group("type") or "tap")
                     if m.group("options"):
                         ifaces[name].options = cast_value(m.group("options"))
                 if not ifaces: