rjarry/sosviz

commit 044b2d276d8e7ab2ded6511eebf947e7224f1237

ip,ovs: add statistics and link state info
Signed-off-by: Robin Jarry <rjarry@redhat.com>

sosviz/output/dot.py
@@ -220,6 +220,18 @@ class SOSGraph:
     def iface_node_id(self, name, netns):
         return f"net_{netns}_{name}"
 
+    NETDEV_ERRORS = {
+        "rx_dropped",
+        "rx_errors",
+        "rx_fifo",
+        "rx_frame",
+        "tx_carrier",
+        "tx_colls",
+        "tx_dropped",
+        "tx_errors",
+        "tx_fifo",
+    }
+
     def phy_iface(self, iface: D, netns: str = ""):
         if netns == "" and iface.name in self.report.ovs.bridges:
             return
@@ -254,6 +266,24 @@ class SOSGraph:
             tooltips.append(f"mac {iface.mac}")
         if "mtu" in iface:
             tooltips.append(f"mtu {iface.mtu}")
+
+        state = []
+        if "DOWN" in iface["flags"]:
+            state.append('<font color="darkorange">DOWN</font>')
+        else:
+            state.append('<font color="forestgreen">UP</font>')
+        if "NO-CARRIER" in iface["flags"]:
+            state.append('<font color="red">NO-CARRIER</font>')
+        elif "LOWER_UP" in iface["flags"]:
+            state.append('<font color="forestgreen">LOWER_UP</font>')
+        labels.append(f"state {','.join(state)}")
+
+        for name, value in iface.get("stats", {}).items():
+            tooltips.append(f"{name} {human_readable(value)}")
+            if "RUNNING" in iface["flags"] and name in self.NETDEV_ERRORS:
+                labels.append(
+                    f'<font color="red">{name} {human_readable(value)}</font>'
+                )
 
         color = "salmon" if netns else "hotpink"
         self.node(
@@ -385,6 +415,25 @@ class SOSGraph:
     def ovs_port_node_id(self, name):
         return f"ovs_port_{name}"
 
+    def ovs_port_state(self, state: str) -> str:
+        if state == "up":
+            return '<font color="forestgreen">up</font>'
+        return '<font color="red">DOWN</font>'
+
+    def ovs_base_labels(self, port: D):
+        labels = [
+            f"<b>{port.name}</b>",
+            f"OVS {port.type}",
+        ]
+        if "admin_state" in port and "link_state" in port:
+            state = ",".join(
+                self.ovs_port_state(s) for s in (port.admin_state, port.link_state)
+            )
+            labels.append(f"state {state}")
+        if "tag" in port:
+            labels.append(f'<font color="forestgreen">VLAN {port.tag}</font>')
+        return labels
+
     def ovs_dpdk_labels(self, port: D):
         if "dpdk_devargs" in port.options:
             numa_id = "N/A"
@@ -410,6 +459,34 @@ class SOSGraph:
             s = "s" if len(disabled) > 1 else ""
             yield f'<font color="gray">rxq{s} {bit_list(disabled)} disabled</font>'
 
+    OVS_ERRORS = {
+        "collisions",
+        "ovs_rx_qos_drops",
+        "ovs_tx_failure_drops",
+        "ovs_tx_invalid_hwol_drops",
+        "ovs_tx_mtu_exceeded_drops",
+        "ovs_tx_qos_drops",
+        "rx_crc_err",
+        "rx_dropped",
+        "rx_errors",
+        "rx_frame_err",
+        "rx_missed_errors",
+        "rx_over_err",
+        "tx_dropped",
+        "tx_errors",
+    }
+
+    def ovs_stats_labels(self, port: D):
+        for name, value in port.get("stats", {}).items():
+            if value and port.get("admin_state") == "up" and name in self.OVS_ERRORS:
+                yield f'<font color="red">{name} {human_readable(value)}</font>'
+
+    def ovs_stats_tooltip(self, port: D):
+        tip = []
+        for name, value in port.get("stats", {}).items():
+            tip.append(f"{name} {human_readable(value)}")
+        return tip
+
     def ovs_port(self, port: D):
         if port.name in self.report.interfaces:
             self.edge(
@@ -419,10 +496,7 @@ class SOSGraph:
             )
             return
 
-        labels = [
-            f"<b>{port.name}</b>",
-            f"OVS {port.type}",
-        ]
+        labels = self.ovs_base_labels(port)
         if port.type in ("dpdk", "dpdkvhostuserclient"):
             labels.extend(self.ovs_dpdk_labels(port))
             if "dpdk_devargs" in port.options:
@@ -433,8 +507,6 @@ class SOSGraph:
                     color="darkorange",
                 )
 
-        if "tag" in port:
-            labels.append(f'<font color="forestgreen">VLAN {port.tag}</font>')
         iface = self.report.interfaces.get(port.name, None)
         if iface is not None:
             for ip in self.report.interfaces[port.name].get("ip", []):
@@ -459,10 +531,12 @@ class SOSGraph:
                     style="solid",
                     color="forestgreen",
                 )
+        labels.extend(self.ovs_stats_labels(port))
 
         self.node(
             self.ovs_port_node_id(port.name),
             labels,
+            tooltip=self.ovs_stats_tooltip(port),
             color="forestgreen",
         )
         self.edge(
@@ -482,10 +556,7 @@ class SOSGraph:
                     )
                     continue
 
-                labels = [
-                    f"<b>{member.name}</b>",
-                    f"OVS {member.type}",
-                ]
+                labels = self.ovs_base_labels(member)
                 if member.type == "dpdk":
                     labels.extend(self.ovs_dpdk_labels(member))
                     self.edge(
@@ -494,9 +565,11 @@ class SOSGraph:
                         style="dashed",
                         color="darkorange",
                     )
+                labels.extend(self.ovs_stats_labels(member))
                 self.node(
                     self.ovs_port_node_id(member.name),
                     labels,
+                    tooltip=self.ovs_stats_tooltip(member),
                     color="forestgreen",
                 )
                 self.edge(

sosviz/collect/ip.py
@@ -53,23 +53,24 @@ IFACE_BLOCK_RE = re.compile(r"^\d+:\s.*\n(\s{4}.+\n)+", re.VERBOSE | re.MULTILIN
 
 
 def parse_report(path: pathlib.Path, data: D):
+    stats = get_netdev_stats(path)
     data.interfaces = parse_interfaces(
-        path / "sos_commands/networking/ip_-d_address", path
+        path / "sos_commands/networking/ip_-d_address", path, stats
     )
     data.netns = D()
     for ip_addr in path.glob(
         "sos_commands/networking/namespaces/*/*_ip_-d_address_show"
     ):
         netns = re.sub(r"ip_netns_exec_(.*)_ip_-d_address_show", r"\1", ip_addr.name)
-        data.netns[ip_addr.parent.name] = parse_interfaces(ip_addr, path)
+        data.netns[ip_addr.parent.name] = parse_interfaces(ip_addr, path, D())
     for ip_addr in path.glob(
         "sos_commands/networking/ip_netns_exec_*_ip*_address_show"
     ):
         netns = re.sub(r"ip_netns_exec_(.*)_ip.*_address_show", r"\1", ip_addr.name)
-        data.netns[netns] = parse_interfaces(ip_addr, path)
+        data.netns[netns] = parse_interfaces(ip_addr, path, D())
 
 
-def parse_interfaces(ip_addr: pathlib.Path, path: pathlib.Path) -> D:
+def parse_interfaces(ip_addr: pathlib.Path, path: pathlib.Path, stats: D) -> D:
     ifaces = D()
     if not ip_addr.is_file():
         return ifaces
@@ -83,4 +84,48 @@ def parse_interfaces(ip_addr: pathlib.Path, path: pathlib.Path) -> D:
         ifaces[d.name] = d
         for m in ADDR_RE.finditer(block.group()):
             d.setdefault("ip", []).append(m.group("addr"))
+        if d.name in stats:
+            d.stats = stats[d.name]
     return ifaces
+
+
+STATS_RE = re.compile(
+    r"""
+    ^
+    \s*
+    (?P<name>[^:]+):\s+
+    (?P<rx_bytes>\d+)\s+
+    (?P<rx_packets>\d+)\s+
+    (?P<rx_errors>\d+)\s+
+    (?P<rx_dropped>\d+)\s+
+    (?P<rx_fifo>\d+)\s+
+    (?P<rx_frame>\d+)\s+
+    (?P<rx_compressed>\d+)\s+
+    (?P<rx_multicast>\d+)\s+
+    (?P<tx_bytes>\d+)\s+
+    (?P<tx_packets>\d+)\s+
+    (?P<tx_errors>\d+)\s+
+    (?P<tx_dropped>\d+)\s+
+    (?P<tx_fifo>\d+)\s+
+    (?P<tx_colls>\d+)\s+
+    (?P<tx_carrier>\d+)\s+
+    (?P<tx_compressed>\d+)
+    \s*
+    $
+    """,
+    re.VERBOSE | re.MULTILINE,
+)
+
+
+def get_netdev_stats(path: pathlib.Path) -> D:
+    stats = D()
+    dev = path / "proc/net/dev"
+    if not dev.is_file():
+        return stats
+
+    for match in STATS_RE.finditer(dev.read_text()):
+        dic = match.groupdict()
+        name = dic.pop("name")
+        stats[name] = D({k: int(v) for k, v in dic.items()})
+
+    return stats

sosviz/collect/ovs.py
@@ -75,7 +75,7 @@ def ovs_ports(ovs, path):
                 if not ifaces:
                     continue
                 port_name = strip_quotes(match.group("name"))
-                port = D(name=port_name, bridge=br_name)
+                port = D(name=port_name, bridge=br_name, stats=D())
                 if len(ifaces) == 1 and port_name in ifaces:
                     port.update(ifaces[port_name])
                 else:
@@ -89,6 +89,23 @@ def ovs_ports(ovs, path):
                     br_name, D(name=br_name, ports=0, of_rules=0)
                 ).ports += 1
                 bridges[br_name].datapath = datapath
+
+    for f in path.glob("sos_commands/openvswitch/ovs-vsctl*_list_interface"):
+        for block in f.read_text().split("\n\n"):
+            d = {}
+            ovs_to_dict(block, d)
+            if d.get("name") in ovs.ports:
+                p = ovs.ports[d["name"]]
+            else:
+                for port in ovs.ports.values():
+                    if "members" in port and d["name"] in port.members:
+                        p = port.members[d["name"]]
+                        break
+                else:
+                    continue
+            p.admin_state = d.get("admin_state", "?")
+            p.link_state = d.get("link_state", "?")
+            p.stats = D(d.get("statistics", {}))
 
     for name, br in bridges.items():
         for f in path.glob(f"sos_commands/openvswitch/ovs-ofctl*_dump-flows_{name}"):

commit 722d05703f86352b7e39e06331f9e1f97c54dea5

dot: error stats reporting on linux netdev
Fixes: 044b2d276d8e ("ip,ovs: add statistics and link state info")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

sosviz/output/dot.py
@@ -280,7 +280,7 @@ class SOSGraph:
 
         for name, value in iface.get("stats", {}).items():
             tooltips.append(f"{name} {human_readable(value)}")
-            if "RUNNING" in iface["flags"] and name in self.NETDEV_ERRORS:
+            if value and "LOWER_UP" in iface["flags"] and name in self.NETDEV_ERRORS:
                 labels.append(
                     f'<font color="red">{name} {human_readable(value)}</font>'
                 )